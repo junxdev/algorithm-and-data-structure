b0eadbd375d246e62492323bab095bda05b3881a
Jun Park <junxdev@gmail.com>
2024-04-01 18:37:21 +0900

    finish linked list stack

---
+++ ch2/LinkedListStack/LinkedListStack.h
@@ -0,0 +1,30 @@
+#ifndef LINKEDLISTSTACK_H
+#define LINKEDLISTSTACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct tagNode
+{
+  char *Data;
+  struct tagNode *NextNode;
+} Node;
+
+typedef struct tagLinkedListStack
+{
+  Node *List;
+  Node *Top;
+} LinkedListStack;
+
+void LLS_CreateStack(LinkedListStack **Stack);
+void LLS_DestroyStack(LinkedListStack *Stack);
+Node *LLS_CreateNode(char *NewData);
+void LLS_DestroyNode(Node *Node);
+void LLS_Push(LinkedListStack *Stack, Node *NewNode);
+Node *LLS_Pop(LinkedListStack *Stack);
+Node *LLS_Top(LinkedListStack *Stack);
+int LLS_GetSize(LinkedListStack *Stack);
+int LLS_IsEmpty(LinkedListStack *Stack);
+
+#endif
